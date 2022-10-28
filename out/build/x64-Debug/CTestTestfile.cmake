01c29ca2d4831319256bc75c350b8ec26f4e6f60
Matsi13 <3291214431@qq.com>
2022-10-28 10:56:26 +0800

    local change

---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/VS projects/cost_model/cost_model
+# Build directory: D:/VS projects/cost_model/cost_model/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
